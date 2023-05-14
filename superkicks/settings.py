96f7de12376e11dbf20b014785434b7aefddeb28
AshwinSajiKumar <118035663+AshwinSajiKumar@users.noreply.github.com>
2023-05-14 15:10:50 +0530

    Updated allowed host

--- superkicks/settings.py
+++ superkicks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z9&c7rc+1(w+5v5*7i$+-l!p-c#9&-coj=r=sxee9^ugwbr+(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AshwinSajiKumar.pythonanywhere.com']
 
 
 # Application definition
